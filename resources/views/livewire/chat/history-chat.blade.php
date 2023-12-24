44cb0c5e4b85c9d708dc1f2f8736f4c513b2d3e7
adhinnugroho <adhinnnugroho@gmail.com>
2023-12-24 21:44:43 +0700

    update setting for delete message in javascript

--- resources/views/livewire/chat/history-chat.blade.php
+++ resources/views/livewire/chat/history-chat.blade.php
@@ -17,8 +17,8 @@
             <i class="fas fa-plus text-2xl text-gray-500 mt-1"></i>
 
             <textarea class="bg-gray-300 rounded-lg w-[63rem] px-4 py-2 focus:border-gray-300" type="text"
-                placeholder="Ketik pesan Anda..." id="send_message" x-ref="input" x-model="inputValue"
-                wire:model.lazy="chatvalue" @keydown.enter="submitForm" x-on:keyup="adjustInputHeight">
+                placeholder="Ketik pesan Anda..." id="send_message" x-ref="input" x-model="inputValue" wire:model.lazy="chatvalue"
+                @keydown.enter="submitForm" x-on:keyup="adjustInputHeight">
 
             </textarea>
             <i class="fas fa-microphone text-2xl float-right mt-1 fixed right-7 text-gray-500 cursor-pointer"></i>
@@ -40,29 +40,40 @@
                 var cursorPosition = input.value.length;
                 var threshold = 10;
                 var chunkSize = 50;
+                if (event.key === "Backspace" || event.keyCode === 8) {
+                    // Handle backspace key
+                    if (cursorPosition === 0) {
+                        // Jika kursor berada di awal, hapus baris sebelumnya
+                        var lines = input.value.split('\n');
+                        var currentLine = lines[cursorPosition];
+                        var previousLine = lines[cursorPosition - 1] || '';
 
-                if ((cursorPosition * 4) + 50 >= ((inputWidth / 2) - threshold)) {
+                        input.value = lines.slice(0, cursorPosition - 1).join('\n') + previousLine + currentLine;
+                        input.setSelectionRange(previousLine.length, previousLine.length);
+                    }
+                } else if ((cursorPosition * 4) + 50 >= ((inputWidth / 2) - threshold)) {
                     // Memecah teks menjadi dua bagian: sebelum dan setelah posisi kursor
-                    var beforeInsert = input.value.substring(0, (cursorPosition * 4) + 52);
-                    var afterInsert = input.value.substring((cursorPosition * 4) + 52);
+                    var beforeInsert = input.value.substring(0, cursorPosition);
+                    var afterInsert = input.value.substring(cursorPosition);
 
                     // Menemukan posisi newline sebelumnya
                     var lastNewline = beforeInsert.lastIndexOf('\n');
                     // Menemukan posisi awal dari blok teks terakhir yang ditambahkan
-                    var startOfLastBlock = lastNewline + (cursorPosition * 4) + 53;
+                    var startOfLastBlock = lastNewline + 1;
 
                     // Menghitung posisi kursor setelah penambahan newline
-                    var newPosition = (cursorPosition * 8) + 51;
+                    var newPosition = cursorPosition + 1;
 
                     // Jika kita sudah melebihi chunkSize, geser kursor ke awal blok terakhir
-                    if ((cursorPosition * 8) + 50 - startOfLastBlock >= chunkSize) {
-                        newPosition = startOfLastBlock;
+                    if (((cursorPosition * 4) + 50) - startOfLastBlock >= ((cursorPosition * 4) + 50)) {
+                        // Menyesuaikan posisi kursor berdasarkan newline terakhir
+                        newPosition = startOfLastBlock + ((cursorPosition * 4) + 50) + 1;
                     }
 
-                    // // Menambahkan newline pada posisi yang dihitung
+                    // Menambahkan newline pada posisi yang dihitung
                     input.value = beforeInsert.substring(0, newPosition) + '\n' + afterInsert;
 
-                    // // Menetapkan posisi kursor yang benar setelah penyisipan newline
+                    // Menetapkan posisi kursor yang benar setelah penyisipan newline
                     input.setSelectionRange(newPosition + 1, newPosition + 1);
 
                     // Menyesuaikan tinggi input
